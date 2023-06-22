d54b9d63e054ba40e626f3e62185fd6b5dcc6629
3Harang <jungjaeseung9475@gmail.com>
2023-06-22 14:30:12 +0900

    # 1.1
    Vanilla Redux로 만든 Counter

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
+import { legacy_createStore } from "redux";
+
 const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case "ADD":
+      return count + 1;
+    case "MINUS":
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
+const countStore = legacy_createStore(countModifier);
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+countStore.subscribe(() => (number.innerText = countStore.getState()));
 
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+plus.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
